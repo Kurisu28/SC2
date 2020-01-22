fe51b758fb0582dfd8b84e5cdc610404d005ae92
Kurisu28 <naussica1128@gmail.com>
2020-01-22 17:19:39 -0600

    html_angular_topology_practice

--- src/app/routing/routers.ts
+++ src/app/routing/routers.ts
@@ -9,7 +9,8 @@ import { DishdetailComponent } from '../dishdetail/dishdetail.component';
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'menu', component: MenuComponent },
-    { path: 'dishdetail/:id', component: DishdetailComponent },
+    { path: 'about', component: AboutComponent},
     { path: 'contact', component: ContactComponent },
+    { path: 'dishdetail/:id', component: DishdetailComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
